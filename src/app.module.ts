df0be4570237331d705240ae3b61792c6f8d299a
Ahmed Hamdy <hamdimn54@gmail.com>
2023-08-18 03:25:18 +0300

    [AppModule] - add global exception handler - add joi config schema - add utils - register config module - register mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GlobalHandler } from './shared/exception-handlers';
+import { configSchema } from './shared/schemas/joi';
+import { mongooseFactory } from './shared/utils';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validationSchema: configSchema() }),
+    MongooseModule.forRootAsync({ inject: [ConfigService], useFactory: mongooseFactory }),
+  ],
+  providers: [{ provide: APP_FILTER, useClass: GlobalHandler }],
 })
 export class AppModule {}
---
+++ src/shared/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './env.util';
+export * from './mongoose-factory.util';
